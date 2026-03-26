ab2b92ccb90d3680bfca1d6e84a31c6cd95a8a91
Jung-Choi-01 <jungchoi2023+github.com@gmail.com>
2026-03-26 17:12:04 -0400

    add functionality to release to buffer with actions secrets

---
+++ release_to_buffer.py
@@ -0,0 +1,66 @@
+import argparse
+import requests
+
+
+def main(api_token: str, channel_id: str, post_time: str, media_url: str, text: str) -> None:
+    url = "https://api.buffer.com"
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {api_token}",
+    }
+
+    payload = {
+        "query": """
+mutation CreatePost($channelId: ChannelId!, $postTime: DateTime, $url: String!, $text: String!) {
+    createPost(input: {
+        text: $text,
+        channelId: $channelId,
+        schedulingType: automatic,
+        mode: customScheduled,
+        dueAt: $postTime,
+        assets: {
+        videos:[
+            {
+            url: $url
+            }
+        ]
+        }
+    }) {
+        ... on PostActionSuccess {
+        post {
+            id
+            text
+            assets {
+            id
+            mimeType
+            }
+        }
+        }
+        ... on MutationError {
+        message
+        }
+    }
+}
+            """,
+        "variables": {
+            "channelId": channel_id,
+            "postTime": post_time,
+            "url": media_url,
+            "text": text,
+        },
+    }
+
+    response = requests.request("POST", url, headers=headers, json=payload)
+    print(response.text)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Post a scheduled Buffer video update")
+    parser.add_argument("api_token", help="Buffer API token")
+    parser.add_argument("channel_id", help="Buffer channel id")
+    parser.add_argument("post_time", help="Scheduled post time in ISO 8601 format")
+    parser.add_argument("media_url", help="URL of video asset")
+    parser.add_argument("text", help="Plaintext string for post text")
+
+    args = parser.parse_args()
+    main(args.api_token, args.channel_id, args.post_time, args.media_url, args.text)
