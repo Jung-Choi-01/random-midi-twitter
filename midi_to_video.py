426a2990c574e80dd8ba39ac086f5b64b4e33ddc
Jung-Choi-01 <jungchoi2023+github.com@gmail.com>
2026-03-26 22:23:19 -0400

    video updates and post text updates

--- midi_to_video.py
+++ midi_to_video.py
@@ -1,10 +1,15 @@
 from synthviz import create_video
+from colorsys import hsv_to_rgb
+import random
 
-# possible future todo's
-# - randomize colors
+def random_rgb():
+    return hsv_to_rgb(
+        random.random(), 
+        random.uniform(0.6, 1.0), 
+        random.uniform(0.6, 0.8))
 
 def write_to_video(source_midi: str, output_file: str):
     create_video(input_midi=source_midi,
                  video_filename=output_file,
-                 falling_note_color=[255, 255, 255],
-                 vertical_speed=1)
+                 falling_note_color=random_rgb,
+                 vertical_speed=.5)
